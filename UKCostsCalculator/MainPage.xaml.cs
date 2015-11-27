580a6483040647c5ea52ddbbdc1715f08d04f365
emilyperegrine <tomfperegrine@googlemail.com>
2015-11-27 19:04:36 +0000

    Initial Commit

---
+++ UKCostsCalculator/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UKCostsCalculator
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+
+		void OnSettingsBtnClicked(object sender, EventArgs args) {
+			DisplayAlert ("Settings", "Settings have not yet been implemented", "OK");
+		}
+
+		void OnTaxBtnClicked(object sender, EventArgs args) {
+			DisplayAlert ("Tax Calculation", "Tax Calculation has not yet been implemented", "OK");
+		}
+	}
+}
+
